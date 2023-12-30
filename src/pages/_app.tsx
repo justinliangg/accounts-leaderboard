94bca0986e513f9f8cc5539f41b1929ef6a397bf
Justin Liang <justin4426@outlook.com>
2023-12-31 00:22:17 +0800

    Add next js analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <main className="text-primary">
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </QueryClientProvider>
   );
