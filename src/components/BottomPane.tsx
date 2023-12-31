9d6889934f84f790ade6de0233291ef93c2aabdf
Justin Liang <justin4426@outlook.com>
2023-12-31 20:22:58 +0800

    fix vh issue on ios

--- src/components/BottomPane.tsx
+++ src/components/BottomPane.tsx
@@ -9,8 +9,8 @@ const BottomPane = ({ open, children }: BottomPaneProps) => {
   return (
     <div
       className={`fixed bottom-0 left-0 right-0 ${
-        open ? "h-[99vh]" : "h-0"
-      } bg-white overflow-hidden transition-height duration-500 ease-in-out border-t rounded-[20px] border-gray-100`}
+        open ? "h-[100%]" : "h-0"
+      } bg-white overflow-hidden transition-height duration-500 ease-in-out border-t border-gray-100`}
     >
       <div>{children}</div>
     </div>
