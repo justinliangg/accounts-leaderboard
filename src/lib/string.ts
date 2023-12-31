1611ebd7a8c38ad28502c0daf5e536bac6bb8044
Justin Liang <justin4426@outlook.com>
2023-12-31 20:38:01 +0800

    Remove period in for categories when displayed

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -2,5 +2,6 @@ export const camelToNormal = (value: string) => {
   return value
     .replace(/([A-Z])/g, (match) => ` ${match}`)
     .replace(/^./, (match) => match.toUpperCase())
+    .replace(".", "")
     .trim();
 };
