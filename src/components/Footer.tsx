0e086f765fc0950d59a4ced08d6a12e0de00a0f4
Justin Liang <justin4426@outlook.com>
2023-12-31 22:12:09 +0800

    feat: add footer for recognition

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="bg-background p-3 fixed bottom-0 w-full border-t-2">
+      <div className="container mx-auto text-center">
+        <p>
+          Data sourced from Techt&apos;s{" "}
+          <Link
+            className="underline"
+            href="https://docs.google.com/spreadsheets/d/145iM6uuFS9m-Rul65--eFJQq_Au7Z_BA4_CwkYwu2DI/edit#gid=0"
+          >
+            spreadsheet
+          </Link>{" "}
+          &#x2665;
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import Footer from "@/components/Footer";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="text-primary min-h-screen max-w-screen bg-background py-[10px] px-5 flex justify-center">
+      <main className="text-primary min-h-screen max-w-screen bg-background py-[10px] pb-14 px-5 flex justify-center">
         <Component {...pageProps} />
+        <Footer />
         <Analytics />
       </main>
     </QueryClientProvider>
