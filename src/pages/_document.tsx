5350fba09ae92eb83c411396193ebeeac2ad38aa
Justin Liang <justin4426@outlook.com>
2024-01-01 18:23:51 +0800

    fix: issue with mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title="Savings Accounts Leaderboard">
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <Head title="Savings Accounts Leaderboard"></Head>
       <body>
         <Main />
         <NextScript />
