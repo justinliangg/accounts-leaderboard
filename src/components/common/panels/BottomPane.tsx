751ffeeaf27eac39a06a927b391595160991b1a8
Justin Liang <justin4426@outlook.com>
2024-01-01 17:00:27 +0800

    fix: weird border at footer

--- src/components/common/panels/BottomPane.tsx
+++ src/components/common/panels/BottomPane.tsx
@@ -17,8 +17,8 @@ const BottomPane = ({ open, children }: BottomPaneProps) => {
 
   return (
     <div
-      className={`fixed bottom-0 left-0 right-0 ${
-        open ? "h-[100%]" : "h-0"
+      className={`fixed bottom-0 left-0 right-0 h-[100%] ${
+        open ? "translate-y-0" : "translate-y-full"
       } bg-white overflow-hidden transition-all duration-500 ease-in-out border-t border-gray-100 pb-14`}
     >
       <div>{children}</div>
