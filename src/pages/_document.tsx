a2d803b0a95fa6e922ab726498a47aee44382e72
Justin Liang <justin4426@outlook.com>
2024-01-01 14:59:11 +0800

    fix: favicon working again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="Savings Accounts Leaderboard">
+        <meta name="theme-color" content="#ff8000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
